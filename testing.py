9f24bc239906f1305614e2262388557cc2a49472
summerela <summerrae78@gmail.com>
2015-08-24 17:06:29 -0700

    finish fixing logic to search for rare OR pathogenic. add results back to impala and put more workload on impala.?

--- testing.py
+++ testing.py
@@ -1,23 +1,5 @@
-#################
-# load modules  #
-#################
 import pandas as pd
-from impala.dbapi import connect
-from impala.udf import udf, ship_udf
-from string import split
 
-#create database connection
-conn = connect(host='glados19', port=21050)
-cur = conn.cursor()
+cand_script = pd.DataFrame(open('/Users/selasady/impala_annot/candidate_patterns_for_itmi.103-00001_at_0713190239.txt', 'rw').readlines())
 
-
-
-
-@udf
-def hour_from_weird_data_format(context, date):
-    return split(date, '-')[1]
-
-ship_udf(cur, hour_from_weird_data_format, '/users_selasady/test_udf.ll', 'glados19')
-
-cur.execute('SELECT hour_from_weird_data_format(date) AS hour FROM public_hg19.clinvar LIMIT 100')
-cur.fetchall()
+print cand_script.head(15)
