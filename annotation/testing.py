cb6eec615ba070365016c22c8db081ee00cb4f78
summerela <summerrae78@gmail.com>
2015-12-04 13:17:27 -0800

    completed testing script, now test

--- annotation/testing.py
+++ annotation/testing.py
@@ -1,6 +1,6 @@
-##############################################
-## update the following variables, then run ##
-##############################################
+##############################################################
+## update the following variables before running the script ##
+##############################################################
 # setup impala and hdfs connections
 impala_host = 'glados14'
 impala_port_number = '21050'
@@ -17,17 +17,27 @@ out_name = 'test_vars'
 # path to place file on hdfs
 hdfs_path = '/user/selasady/'
 
-java_path = 'java'
-gatk_jar =  '/Users/selasady/tools/GenomeAnalysisTK.jar'
-ref_fasta = '/Users/selasady/tools/human_g1k_v37.fasta'
-snpeff_jar = '/Users/selasady/tools/snpEff/snpEff.jar'
-snpeff_oneperline_perl = '/Users/selasady/tools/snpEff/scripts/vcfEffOnePerLine.pl'
-snpsift_jar = '/Users/selasady/tools/snpEff//SnpSift.jar'
-chrom_splitter = '/Users/selasady/tools/snpEff/scripts/splitChr.pl'
+# work mac
+# java_path = 'java'
+# gatk_jar =  '/Users/selasady/tools/GenomeAnalysisTK.jar'
+# ref_fasta = '/Users/selasady/tools/human_g1k_v37.fasta'
+# snpeff_jar = '/Users/selasady/tools/snpEff/snpEff.jar'
+# snpeff_oneperline_perl = '/Users/selasady/tools/snpEff/scripts/vcfEffOnePerLine.pl'
+# snpsift_jar = '/Users/selasady/tools/snpEff//SnpSift.jar'
+# chrom_splitter = '/Users/selasady/tools/snpEff/scripts/splitChr.pl'
 
-#######################
-## create connection ##
-#######################
+# home pc
+java_path = 'java'
+gatk_jar =  'D:/Documents/tools/GenomeAnalysisTK.jar'
+ref_fasta = 'D:/Documents/tools/human_g1k_v37.fasta'
+snpeff_jar = 'D:/Documents/tools/snpEff/snpEff.jar'
+snpeff_oneperline_perl = 'D;/Documents/tools/snpEff/scripts/vcfEffOnePerLine.pl'
+snpsift_jar = 'D:/Documents/tools/snpEff//SnpSift.jar'
+chrom_splitter = 'D:/Documents/tools/snpEff/scripts/splitChr.pl'
+
+#################################
+## create connection to impala ##
+#################################
 print "Creating a connection to impala.. \n"
 
 import ibis
@@ -37,6 +47,8 @@ import pandas as pd
 from impala.dbapi import connect
 import time
 import csv
+import subprocess
+
 
 # disable extraneous pandas warning
 pd.options.mode.chained_assignment = None
@@ -53,14 +65,17 @@ con = ibis.impala.connect(host=impala_host, port=impala_port_number, timeout=120
 # enable interactive mode
 ibis.options.interactive = True
 
+#################################################
+## create vcf files by row for each chromosome ##
+#################################################
 # create vcf header
 def create_header(outfile_name):
    # create vcf header
     lines=[]
     lines.append('##fileformat=VCFv4.0')
     lines.append('##fileDate='+ time.strftime("%y%m%d"))
-    lines.append('##reference=grch37 v.74 \n')
-    lines.append('CHROM \t' + 'POS\t' + 'ID\t' + 'REF\t' + 'ALT\t' + 'QUAL\t'+ 'FILTER\t' + 'INFO\t' + '\n')
+    lines.append('##reference=grch37 v.74')
+    lines.append('#CHROM\t' + 'POS\t' + 'ID\t' + 'REF\t' + 'ALT\t' + 'QUAL\t'+ 'FILTER\t' + 'INFO\t' + 'FORMAT\t' + 'SAMPLE\t' + '\n')
     header = '\n'.join(lines)
     out = open(outfile_name, 'wb')
     out.write(header)
@@ -71,40 +86,110 @@ def create_vcf(db_name, table_name, chrom_name):
      vcf_out = 'chr' + chrom_name + '_' + out_name + '.vcf'
      create_header(vcf_out)
      # connect to vars_to_snpeff table
-     get_vars = "SELECT chrom, pos, id, ref, alt, qual, filter, info from {}.{} WHERE chrom = '{}'".format(input_db, input_table, chrom_name)
+     get_vars = "SELECT chrom, pos, id, ref, alt, qual, filter, info, form, sample from {}.{} WHERE chrom = '{}' order by pos limit 5".format(input_db, input_table, chrom_name)
      cur.execute(get_vars)
      for row in cur:
          with open(vcf_out, 'a') as csvfile:
-             writer = csv.writer(csvfile, delimiter="\t")
+             writer = csv.writer(csvfile, delimiter="\t", lineterminator = '\n')
              writer.writerow(row)
 
-chroms = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'M', 'MT']
-for chrom in chroms:
-    create_vcf(input_db, input_table, chrom)
+#chroms = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'M', 'MT']
+chroms = ['1','2','3']
 
-
-
-
-
-
-# chroms = ['1','2','3']
 # for chrom in chroms:
-#     print "Downloading variants in chromosome {}".format(chrom)
-#     distinct_df = create_vcf(input_table, input_db, chrom)
-#     vcf_out = 'chr' + chrom + '_' + out_name + '.vcf'
-#     print "Formatting chromsome {} as VCF file... \n".format(chrom)
-#     create_header(vcf_out)
-#     impala_to_vcf(distinct_df, vcf_out)
-#     print "Finished with test"
-    # import subprocess
-    # print "Validating VCF with GATK ValidateVariants... \n"
-    # subprocess.call([java_path, "-Xmx32g", "-jar", gatk_jar, "-T ", \
- 	# "ValidateVariants", "-R", ref_fasta, "-V", vcf_out, "--validationTypeToExclude", "ALL"])
-    # snpeff_out =  'chr' + chrom + '_snpeff.vcf'
-    # print "Running snpeff... \n"
-    # f = open(snpeff_out, "w")
-    # subprocess.call([java_path, "-Xmx32g", "-jar", snpeff_jar, "-t", "-v", "-noStats", "GRCh37.74", vcf_out], stdout=f)
+#     print "Creating VCF files for chromosome {}... \n".format(chrom)
+#     create_vcf(input_db, input_table, chrom)
+
+#############################################################
+## annotate variants with coding consequences using snpeff ##
+#############################################################
+# for chrom in chroms:
+#     print "Annotating coding consequences for chromosome {} with snpeff... \n".format(chrom)
+#     vcf_in = 'chr' + chrom + '_' + out_name + '.vcf'
+#     vcf_out = 'chr' + chrom + '_' + out_name + '_snpeff.vcf'
+#     f = open(vcf_out, "w")
+#     try:
+#         subprocess.call([java_path, "-Xmx16g", "-jar", snpeff_jar, "-t", "-v", "-noStats", "GRCh37.74", vcf_in], stdout=f)
+#     except subprocess.CalledProcessError as e:
+#         print e.output
+
+###########################################################
+## Output SnpEff effects as tsv file, one effect per line ##
+############################################################
+for chrom in chroms:
+    print "Parsing snpeff output for chromosome {}... \n".format(chrom)
+    vcf_in = 'chr' + chrom + '_' + out_name + '_snpeff.vcf'
+    tsv_out = 'chr' + chrom + '_' + out_name + '.tsv'
+    # call processes and pipe
+    snpout_cmd = 'cat {} | {} | {} -jar {} extractFields \
+    - CHROM POS REF ALT "ANN[*].GENE" "ANN[*].GENEID" "ANN[*].EFFECT" "ANN[*].IMPACT" \
+    "ANN[*].FEATURE" "ANN[*].FEATUREID" "ANN[*].BIOTYPE" "ANN[*].RANK" \
+    "ANN[*].HGVS_C" "ANN[*].HGVS_P" > {}'.format(vcf_in, snpeff_oneperline_perl, \
+    java_path, snpsift_jar,tsv_out)
+    ps = subprocess.Popen(snpout_cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    ps.communicate()[0]
+
+####################
+## Remove Header  ##
+####################
+for chrom in chroms:
+    print "Removing header for upload to impala... \n"
+    tsv_in = 'chr' + chrom + '_' + out_name + '.tsv'
+    tsv_out = 'chr' + chrom + '_' + out_name + '_final.tsv'
+    tsv_cmd = "sed '1d' {} | tr '/\t' ',' > {}".format(tsv_in,tsv_out)
+    csv_proc = subprocess.Popen(tsv_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    csv_proc.communicate()[0]
 
 
+#####################################
+## create hdfs directory for files ##
+#####################################
+for chrom in chroms:
+    print "Uploading chromosome {} to HDFS... \n".format(chrom)
+    tsv_out = 'chr' + chrom + '_' + out_name + '_final.tsv'
+    # create directory
+    if hdfs.exists(hdfs_path):
+        hdfs.rmdir(hdfs_path)
+        hdfs.mkdir(hdfs_path)
+        hdfs.put(hdfs_path, tsv_out, verbose=True)
 
+#########################
+## create impala table ##
+#########################
+# define table schema for tsv file
+snpeff_schema = ibis.schema([
+     ('chrom', 'string'),
+     ('pos', 'int32'),
+     ('ref', 'string'),
+     ('alt', 'string'),
+     ('gene', 'string'),
+     ('gene_id', 'string'),
+     ('effect', 'string'),
+     ('impact', 'string'),
+     ('feature', 'string'),
+     ('feature_id', 'string'),
+     ('biotype', 'string'),
+     ('rank', 'int32'),
+     ('hgvs_c', 'string'),
+     ('hgvs_p', 'string')
+])
+
+# create empty table to store snpeff output
+table_name = out_name + '_snpeff'
+con.create(table_name, schema=snpeff_schema)
+
+#connect to empty table for inserting snpeff results
+snpeff_table = con.table(table_name, db=input_db)
+
+###########################
+## upload snpeff results ##
+###########################
+for chrom in chroms:
+    tsv_out = 'chr' + chrom + '_' + out_name + '_final.tsv'
+    file_path = hdfs_path + '/' + tsv_out
+    # create ibis object from  tsv
+    print "Creating impala table for chromosome {}... \n".format(chrom)
+    con.insert(table_name, con.delimited_file(file_path, snpeff_schema, delimiter=','), database=input_db, force=True)
+
+print "Snpeff pipeline complete."
 
