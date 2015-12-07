da6e8870fd5729d29e261673c7ef2f60ef2997b7
summerela <summerrae78@gmail.com>
2015-12-07 12:06:00 -0800

    testing vcf parsing script

--- annotation/snpeff.py
+++ annotation/snpeff.py
@@ -88,7 +88,7 @@ for chrom in chroms:
 for chrom in chroms:
     print "Verifying VCF format for chromosome {}... \n".format(chrom)
     vcf_checked_in =  'chr' + chrom_name + '_' + out_name + '.vcf'
-     vcf_checked_out = 'chr' + chrom_name + '_verified.vcf'
+    vcf_checked_out = 'chr' + chrom_name + '_verified.vcf'
     # create the file and run snpeff
     with open(vcf_checked_out, "w") as out_file:
         try:
