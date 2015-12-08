60b3de4904a25fd707663dd4f4b12b8e965a38b3
summerela <summerrae78@gmail.com>
2015-12-08 12:50:24 -0800

    creating final partitioned table, adding rsid back into dataframe

--- annotation/snpeff.py
+++ annotation/snpeff.py
@@ -110,52 +110,52 @@ chroms = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '
 #         except subprocess.CalledProcessError as e:
 #              print e.output
 #
-# ##########################################################
-# ## Output SnpEff effects as tsv file, one effect per line ##
-# ############################################################
-# for chrom in chroms:
-#     print "Parsing snpeff output for chromosome {}... \n".format(chrom)
-#     vcf_in = 'chr' + chrom + '_' + out_name + '_snpeff.vcf'
-#     tsv_out = 'chr' + chrom + '_' + out_name + '.tsv'
-#     # create command to parse snpeff
-#     snpout_cmd = 'cat {} | {} | {} -jar {} extractFields \
-#     - CHROM POS REF ALT "ANN[*].GENE" "ANN[*].GENEID" "ANN[*].EFFECT" "ANN[*].IMPACT" \
-#     "ANN[*].FEATURE" "ANN[*].FEATUREID" "ANN[*].BIOTYPE" "ANN[*].RANK" \
-#     "ANN[*].HGVS_C" "ANN[*].HGVS_P" > {}'.format(vcf_in, snpeff_oneperline_perl, \
-#     java_path, snpsift_jar,tsv_out)
-#     # call subprocess and communicate to pipe output between commands
-#     ps = subprocess.Popen(snpout_cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-#     print ps.communicate()[0]
-#
-# ####################
-# ## Remove Header  ##
-# ####################
-# # remove header need for running snpeff to create out own column names on impala
-# for chrom in chroms:
-#     print "Removing header for chromosome {} upload to impala... \n".format(chrom)
-#     tsv_in = 'chr' + chrom + '_' + out_name + '.tsv'
-#     tsv_out = 'chr' + chrom + '_' + out_name + '_final.tsv'
-#     tsv_cmd = "sed '1d' {} > {}".format(tsv_in,tsv_out)
-#     tsv_proc = subprocess.Popen(tsv_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#     print tsv_proc.communicate()[0]
+##########################################################
+## Output SnpEff effects as tsv file, one effect per line ##
+############################################################
+for chrom in chroms:
+    print "Parsing snpeff output for chromosome {}... \n".format(chrom)
+    vcf_in = 'chr' + chrom + '_' + out_name + '_snpeff.vcf'
+    tsv_out = 'chr' + chrom + '_' + out_name + '.tsv'
+    # create command to parse snpeff
+    snpout_cmd = 'cat {} | {} | {} -jar {} extractFields \
+    - CHROM POS ID REF ALT "ANN[*].GENE" "ANN[*].GENEID" "ANN[*].EFFECT" "ANN[*].IMPACT" \
+    "ANN[*].FEATURE" "ANN[*].FEATUREID" "ANN[*].BIOTYPE" "ANN[*].RANK" \
+    "ANN[*].HGVS_C" "ANN[*].HGVS_P" > {}'.format(vcf_in, snpeff_oneperline_perl, \
+    java_path, snpsift_jar,tsv_out)
+    # call subprocess and communicate to pipe output between commands
+    ps = subprocess.Popen(snpout_cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    print ps.communicate()[0]
+
+####################
+## Remove Header  ##
+####################
+# remove header need for running snpeff to create out own column names on impala
+for chrom in chroms:
+    print "Removing header for chromosome {} upload to impala... \n".format(chrom)
+    tsv_in = 'chr' + chrom + '_' + out_name + '.tsv'
+    tsv_out = 'chr' + chrom + '_' + out_name + '_final.tsv'
+    tsv_cmd = "sed '1d' {} > {}".format(tsv_in,tsv_out)
+    tsv_proc = subprocess.Popen(tsv_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    print tsv_proc.communicate()[0]
 
 ###############################
-# ## Upload results to hdfs  ##
-# #############################
+## Upload results to hdfs  ##
+#############################
 import datetime
 now = datetime.datetime.now()
 
 # define output path on hdfs
 out_path = "{}snpeff_{}".format(hdfs_path, str(now.strftime("%Y%m%d")))
-# mkdir_cmd = "hdfs dfs -mkdir {}".format(out_path)
-# mkdir_proc = subprocess.Popen(mkdir_cmd, shell=True, stderr=subprocess.STDOUT)
-# print mkdir_proc.communicate()[0]
+mkdir_cmd = "hdfs dfs -mkdir {}".format(out_path)
+mkdir_proc = subprocess.Popen(mkdir_cmd, shell=True, stderr=subprocess.STDOUT)
+print mkdir_proc.communicate()[0]
 
-#  # put each file in the snpeff directory
-# print "Uploading files to HDFS... \n"
-# hdfs_cmd = 'hdfs dfs -put chr*_final.tsv {}'.format(out_path)
-# hdfs_proc = subprocess.Popen(hdfs_cmd, shell=True, stderr=subprocess.STDOUT)
-# print hdfs_proc.communicate()[0]
+ # put each file in the snpeff directory
+print "Uploading files to HDFS... \n"
+hdfs_cmd = 'hdfs dfs -put chr*_final.tsv {}'.format(out_path)
+hdfs_proc = subprocess.Popen(hdfs_cmd, shell=True, stderr=subprocess.STDOUT)
+print hdfs_proc.communicate()[0]
 
 # set read/write permissions on directory
 chown_dir_cmd = "hdfs dfs -chown -R impala:supergroup {}".format(hdfs_path)
@@ -174,6 +174,7 @@ create_coding= '''
 create table {}.coding_consequences
      (chrom string,
       pos int,
+      id string,
       ref string,
       alt string,
       gene string,
@@ -203,10 +204,52 @@ cur.execute(load_query)
 ############################
 # compute stats on table ##
 ############################
-final_compstats = "compute stats {}.coding_consequences".format(input_db)
-cur.execute(final_compstats)
+coding_compstats = "compute stats {}.coding_consequences".format(input_db)
+cur.execute(coding_compstats)
 
-# TODO add script to make table partitioned
+##########################
+# partition final table ##
+##########################
+
+# drop the table if it already exists
+drop_coding = "drop table if exists {}.all_coding".format(input_db)
+cur.execute(drop_coding)
+
+# create partitioned table
+create_parition = '''
+create table {}.all_coding
+    (
+      pos int,
+      id string,
+      ref string,
+      alt string,
+      gene string,
+      gene_id string,
+      effect string,
+      impact string,
+      feature string,
+      feature_id string,
+      biotype string,
+      rank int,
+      hgvs_c string,
+      hgvs_p string
+      )
+partitioned by (chrom string)'''.format(input_db)
+cur.execute(create_parition)
+
+# insert unpartitioned table
+insert_partition = '''
+insert into table {}.all_coding partition(chrom)
+    (
+      select  pos, id, ref, alt, gene, gene_id, effect, impact, feature, feature_id,
+      biotype, rank, hgvs_c, hgvs_p, chrom
+      from p7_product.coding_consequences
+      )'''.format(input_db)
+cur.execute(insert_partition)
+
+# compute stats
+final_compstats = "compute stats {}.all_coding".format(input_db)
+cur.execute(final_compstats)
 
 #####################
 # close connection ##
