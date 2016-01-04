b70d98aed5ad71bc0eac80a5114cb99a42f27417
summerela <summerrae78@gmail.com>
2016-01-04 14:02:26 -0800

    adding function to upload results to hdfs

--- annotation/update_gv.py
+++ annotation/update_gv.py
@@ -1,6 +1,7 @@
 # specify table to check for new variants
 input_db = "p7_product"
 input_table =  "test"
+hdfs_path = "/user/selasady/"
 
 # system paths to required executables
 java_path = '/tools/java/jdk1.7/bin/java'
@@ -119,6 +120,24 @@ def remove_header(out_name):
     tsv_proc = subprocess.Popen(tsv_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print tsv_proc.communicate()[0]
 
+# upload results to hdfs
+def upload_hdfs(out_name):
+    # define output path on hdfs
+    out_path = "{}snpeff_{}".format(hdfs_path, str(now.strftime("%Y%m%d")))
+    mkdir_cmd = "hdfs dfs -mkdir {}".format(out_path)
+    mkdir_proc = subprocess.Popen(mkdir_cmd, shell=True, stderr=subprocess.STDOUT)
+    print mkdir_proc.communicate()[0]
+    # put snpeff file in the newly created directory
+    print "Uploading coding consequences to HDFS... \n"
+    snpeff_file = out_name + '_final.tsv'
+    hdfs_cmd = 'hdfs dfs -put {} {}'.format(snpeff_file, out_path)
+    hdfs_proc = subprocess.Popen(hdfs_cmd, shell=True, stderr=subprocess.STDOUT)
+    print hdfs_proc.communicate()[0]
+    # set read/write permissions on directory
+    chown_dir_cmd = "hdfs dfs -chown -R impala:supergroup {}".format(hdfs_path)
+    chown_proc = subprocess.Popen(chown_dir_cmd, shell=True, stderr=subprocess.STDOUT)
+    print chown_proc.communicate()[0]
+
 # if new variants are found, annotate with snpeff and upload to impala as a table
 if len(new_vars) > 0:
     # print str(len(new_vars)) + " new variant(s) were found. \n"
@@ -130,8 +149,9 @@ if len(new_vars) > 0:
     # run_snpeff("new_vars")
     # print "Parsing snpeff output. \n"
     # parse_snpeff("new_vars")
-    print "Removing header for upload to impala. \n"
-    remove_header("new_vars")
+    # print "Removing header for upload to impala. \n"
+    # remove_header("new_vars")
+    upload_hdfs("new_vars")
     sys.exit("New variants added to global variants table.")
 
 
