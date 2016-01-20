fb40b72f77c461893677771351e3810bf530ee27
summerela <summerrae78@gmail.com>
2016-01-20 12:41:23 -0800

    updating snpeff non-intergenic parsing to include additional fields DISTANCE and ALLELE

--- annotation/snpeff.py
+++ annotation/snpeff.py
@@ -135,46 +135,46 @@ def intergenic_vcf(db_name, table_name, chrom_name):
 # ############################################################
 # # annotate variants with coding consequences using snpeff ##
 # ############################################################
-for file in os.listdir(os.getcwd()):
-    # run intergenic variants through snpeff using 'closest' feature to annotate to nearest gene
-    if file.endswith('intergenic_verified.vcf'):
-        print "Annotating coding consequences for {} with snpeff... \n".format(file)
-        # create names for input and output files
-        vcf_out = str('.'.join(file.split('.')[:-1]) if '.' in file else file) + '_intergenic_snpeff.vcf'
-        # create the file and run snpeff
-        with open(vcf_out, "w") as f:
-            try:
-                subprocess.call([java_path, "-Xmx16g", "-jar", snpeff_jar, "closest", "-t", "-v", "GRCh37.75", file], stdout=f)
-            except subprocess.CalledProcessError as e:
-                 print e.output
-    # run non-intergenic variants through snpeff
-    elif file.endswith(out_name + '_verified.vcf'):
-        print "Annotating coding consequences for {} with snpeff... \n".format(file)
-        # create names for input and output files
-        vcf_out = str('.'.join(file.split('.')[:-1]) if '.' in file else file) + '_snpeff.vcf'
-        # create the file and run snpeff
-        with open(vcf_out, "w") as f:
-            try:
-                subprocess.call([java_path, "-Xmx16g", "-jar", snpeff_jar, "-t", "-v", "GRCh37.75", file], stdout=f)
-            except subprocess.CalledProcessError as e:
-                 print e.output
+# for file in os.listdir(os.getcwd()):
+#     # run intergenic variants through snpeff using 'closest' feature to annotate to nearest gene
+#     if file.endswith('intergenic_verified.vcf'):
+#         print "Annotating coding consequences for {} with snpeff... \n".format(file)
+#         # create names for input and output files
+#         vcf_out = str('.'.join(file.split('.')[:-1]) if '.' in file else file) + '_intergenic_snpeff.vcf'
+#         # create the file and run snpeff
+#         with open(vcf_out, "w") as f:
+#             try:
+#                 subprocess.call([java_path, "-Xmx16g", "-jar", snpeff_jar, "closest", "-t", "-v", "GRCh37.75", file], stdout=f)
+#             except subprocess.CalledProcessError as e:
+#                  print e.output
+#     # run non-intergenic variants through snpeff
+#     elif file.endswith(out_name + '_verified.vcf'):
+#         print "Annotating coding consequences for {} with snpeff... \n".format(file)
+#         # create names for input and output files
+#         vcf_out = str('.'.join(file.split('.')[:-1]) if '.' in file else file) + '_snpeff.vcf'
+#         # create the file and run snpeff
+#         with open(vcf_out, "w") as f:
+#             try:
+#                 subprocess.call([java_path, "-Xmx16g", "-jar", snpeff_jar, "-t", "-v", "GRCh37.75", file], stdout=f)
+#             except subprocess.CalledProcessError as e:
+#                  print e.output
 
 # ##########################################################
 # ## Output SnpEff effects as tsv file, one effect per line ##
 # ############################################################
-# for file in os.listdir(os.getcwd()):
-#     if file.endswith('_snpeff.vcf'):
-#         print "Parsing snpeff output for {}... \n".format(file)
-#         tsv_out = str('.'.join(file.split('.')[:-1]) if '.' in file else file) + '_parsed.tsv'
-#         # create command to parse snpeff
-#         snpout_cmd = 'cat {} | {} | {} -jar {} extractFields \
-#         - CHROM POS ID REF ALT "ANN[*].GENE" "ANN[*].GENEID" "ANN[*].EFFECT" "ANN[*].IMPACT" \
-#         "ANN[*].FEATURE" "ANN[*].FEATUREID" "ANN[*].BIOTYPE" "ANN[*].RANK" \
-#         "ANN[*].HGVS_C" "ANN[*].HGVS_P" > {}'.format(file, snpeff_oneperline_perl, \
-#         java_path, snpsift_jar,tsv_out)
-#         # call subprocess and communicate to pipe output between commands
-#         ps = subprocess.Popen(snpout_cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-#         print ps.communicate()[0]
+for file in os.listdir(os.getcwd()):
+    if file.endswith('_snpeff.vcf'):
+        print "Parsing snpeff output for {}... \n".format(file)
+        tsv_out = str('.'.join(file.split('.')[:-1]) if '.' in file else file) + '_parsed.tsv'
+        # create command to parse snpeff
+        snpout_cmd = 'cat {} | {} | {} -jar {} extractFields \
+        - CHROM POS ID REF ALT "ANN[*].GENE" "ANN[*].GENEID" "ANN[*}.ALLELE" "ANN[*].EFFECT" "ANN[*].IMPACT" \
+        "ANN[*].FEATURE" "ANN[*].FEATUREID" "ANN[*].BIOTYPE" "ANN[*].RANK" "ANN[*].DISTANCE" \
+        "ANN[*].HGVS_C" "ANN[*].HGVS_P" > {}'.format(file, snpeff_oneperline_perl, \
+        java_path, snpsift_jar,tsv_out)
+        # call subprocess and communicate to pipe output between commands
+        ps = subprocess.Popen(snpout_cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        print ps.communicate()[0]
 
 # for file in os.listdir(os.getcwd()):
 #     if file.endswith('intergenic_snpeff.vcf'):
