f9972f5bf6c05c746176473941b8016d6658cfcc
summerela <summerrae78@gmail.com>
2016-04-10 22:24:06 -0700

    updating spark connect

--- testing/connect_spark.py
+++ testing/connect_spark.py
@@ -1,68 +1,83 @@
+#!/usr/bin/env python
+
 import os
 import sys
 import subprocess as sp
 
+# Path for spark source folder
+os.environ['SPARK_HOME']="/opt/cloudera/parcels/CDH/lib/spark"
+# Append pyspark  to Python Path
+sys.path.append("/opt/cloudera/parcels/CDH/lib/spark/python")
+
+from pyspark import SparkContext, SparkConf
 
 ########################
 ### Connect to spark ###
 ########################
-def subprocess_cmd(command, input_dir):
-    '''
-    Run programs in bash via subprocess
-    :param command: command string as would be run on the command line
-    :param input_dir: optional directory to run command in, default cwd
-    :return: runs bash command
-    '''
-    print ("Running \n {}".format(command))
-    ps = sp.Popen(command, shell=True,stdout=sp.PIPE,stderr=sp.PIPE, cwd=input_dir)
-    try:
-       print ps.communicate()
-    except sp.CalledProcessError as e:
-         print e
 
-def hdfs_put(input_file, output_dir):
-    hdfs_mkdir_cmd = "hdfs dfs -mkdir {}".format(output_dir)
-    hdfs_put_cmd = "hdfs dfs -put {} {}".format(input_file, output_dir)
-    subprocess_cmd(hdfs_mkdir_cmd, os.getcwd())
-    subprocess_cmd(hdfs_put_cmd, os.getcwd())
+class spark(object):
 
-# Path for spark source folder
-os.environ['SPARK_HOME']="/opt/cloudera/parcels/CDH/lib/spark"
+    def __init__(self, local_dir='./', hdfs_dir='/users/selasady/', master='master', appname='spark_job', spark_mem=2):
+        self.local_dir = local_dir
+        self.hdfs_dir = hdfs_dir
+        self.master = master
+        self.appname = appname
+        self.spark_mem = int(spark_mem)
+        self.conf = (SparkConf()
+               .setMaster(self.master)
+               .setAppName(self.appname)
+               .set("spark.executor.memory", self.spark_mem))
+        # for dependencies, add pyFiles=['file1.py','file2'] argument
+        self.sc = SparkContext(conf=self.conf)
 
-# Append pyspark  to Python Path
-sys.path.append("/opt/cloudera/parcels/CDH/lib/spark/python")
+    def subprocess_cmd(self, command, local_dir):
+        '''
+        Run programs in bash via subprocess
+        :param command: command string as would be run on the command line
+        :param local_dir: optional directory to run command in, default cwd
+        :return: runs bash command
+        '''
+        print ("Running \n {}".format(command))
+        ps = sp.Popen(command, shell=True,stdout=sp.PIPE,stderr=sp.PIPE, cwd=local_dir)
+        try:
+           print ps.communicate()
+        except sp.CalledProcessError as e:
+             print e
 
-# try to import pyspark, throw error if..error
-from pyspark import SparkContext
-from pyspark import SparkConf
+    def hdfs_put(self, input_file):
+        hdfs_mkdir_cmd = "hdfs dfs -mkdir {}".format(self.hdfs_dir)
+        hdfs_put_cmd = "hdfs dfs -put {} {}".format(input_file, self.hdfs_dir)
+        self.subprocess_cmd(hdfs_mkdir_cmd, os.getcwd())
+        self.subprocess_cmd(hdfs_put_cmd, os.getcwd())
 
+    def hdfs_read(self, input_dir):
+        lines = self.sc.textFile(input_dir)
+        return lines
 
-conf = (SparkConf()
-           .setMaster("local")
-           .setAppName("test spark")
-           .set("spark.executor.memory", "1g"))
-
-# for dependencies, add pyFiles=['file1.py','file2'] argument
-sc = SparkContext(conf=conf)
-
-################
+###############
 if __name__ == '__main__':
 
-    # create new folder, wont write into existing
-    hdfs_path = '/user/selasady/testing3/'
+    spark_con = spark(os.getcwd(), '/user/selasady/testing3/', "local", "spark_test", 2)
+
     local_file = '/users/selasady/my_titan_itmi/impala_scripts/testing/test/tale_of_two_cities.txt'
 
     # write file to hdfs using sys call to hdfs put
-    hdfs_put(local_file, hdfs_path)   
+    #spark_con.hdfs_put(local_file)
  
-    # testing pyspark by filtering out lines and counting non-empty
-    lines = sc.textFile('/user/selasady/testing2/tale_of_two_cities.txt')
-    non_empty_lines = lines.filter( lambda x: len(x) > 0)
-    print (non_empty_lines.count())
+    # read in a file from hdfs
+    lines = spark_con.hdfs_read(spark_con.hdfs_dir)
 
+
+    ## Examples ##
+    # filter out empty lines
+    non_empty_lines = lines.filter( lambda x: len(x) > 0)
+    # count non-empty lines
+    print (non_empty_lines.count())
     # word count map/reduce example
     words = non_empty_lines.flatMap(lambda x: x.split())
     wordcounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x,y:x+y).map(lambda x:(x[1], x[0])).sortByKey(False)
     print (wordcounts.take(10))
-    
+
+    # close connection
+    spark_con.sc.stop()
     
