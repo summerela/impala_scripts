08a66432b1490e96a9f5ed9ed8ba98506ff29450
summerela <summerrae78@gmail.com>
2016-04-29 14:06:06 -0700

    Re-wrote entirely in sparkSQL so no dependencies. Logic for testing 1-based on hold until reference db loaded. TODO: re-write to work on entire directory as map/reduce job.

--- testing/spark_etl_test.py
+++ testing/spark_etl_test.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env pyspark
 
+'''
+Assumptions:
+Input file is in either tsv or bzip2 format and contains at least the following columns:
+- chrom
+- pos
+- ref
+- alt
+- subject_id
+- sample_id
+
+Requirments:
+
+- /titan/ITMI1/workspaces/users/selasady/impala_scripts/testing/jars/spark-csv_2.10-1.4.0.jar
+- /titan/ITMI1/workspaces/users/selasady/impala_scripts/testing/jars/commons-csv-1.2.jar
+
+To Run:
+
+- Edit main routine
+
+Type the following from command line
+
+/usr/bin/spark-submit --jars /titan/ITMI1/workspaces/users/selasady/impala_scripts/testing/jars/spark-csv_2.10-1.4.0.jar,/titan/ITMI1/workspaces/users/selasady/impala_scripts/testing/jars/commons-csv-1.2.jar spark_etl_test.py  2>/dev/null
+
+
+'''
+
 import os
 import unittest
-import urllib, json
-import time
 
 from pyspark import SparkContext, SparkConf, SQLContext
 
@@ -42,88 +66,100 @@ class TestETL(unittest.TestCase):
         pandas_df = input_df.toPandas()
         return pandas_df
 
-    def check_chrom_empty(self, input_df):
-        assert input_df.where(input_df.chrom.isNull()).count() == 0, "Null values found in chrom column."
+    def check_empty(self, col_name):
+        result = self.sqlContext.sql("""SELECT * FROM spark_df WHERE {} = 'NULL'""".format(col_name))
+        self.assertTrue(result.count() == 0, "Null values found in {} column.".format(col_name))
 
-    def check_ref_empty(self, input_df):
-        assert input_df.where(input_df.ref.isNull()).count() == 0, "Null values found in ref column."
+    def check_chrom_set(self):
+        result = self.sqlContext.sql("""SELECT distinct chrom FROM spark_df ORDER BY CAST(chrom as int)""")
+        self.assertItemsEqual(self.somatic_chr, result.rdd.map(lambda r: r.chrom).collect(),
+                              "Not all somatic chromosomes are loaded {}.".format(result))
 
-    def check_subject_empty(self, input_df):
-        assert input_df.where(input_df.subject_id.isNull()).count() == 0, "Null values found in subject_id column."
+    def check_chrom_count(self):
+        result = self.sqlContext.sql("""
+        SELECT chrom, COUNT(*)
+        FROM spark_df
+        GROUP BY chrom
+        HAVING COUNT(*) < 1000
+        """)
+        # for name, group in input_df['chrom'].groupby(input_df['chrom']):
+        self.assertTrue(result.count() == 0, "Not all chromosomes contain at least 1000 rows.")
 
-    def check_allele_empty(self, input_df):
-        assert input_df.where(input_df.allele.isNull()).count() == 0, "Null values found in allele column."
+    def check_chrom_prefix(self):
+        result = self.sqlContext.sql("""
+            SELECT chrom
+            FROM spark_df
+            WHERE chrom LIKE '*chr*'
+            """)
+        self.assertTrue(result.count() == 0, "The 'chr' prefix was not removed from the chrom column.")
 
-    def check_sample_empty(self, input_df):
-        assert input_df.where(input_df.sample_id.isNull()).count() == 0, "Null values found in sample_id column."
+    def check_chrom_mt(self):
+        result = self.sqlContext.sql("""
+            SELECT chrom
+            FROM spark_df
+            WHERE chrom = 'MT' or chrom = 'mt'
+            """)
+        self.assertTrue(result.count() == 0, "The 'MT' prefix was not changed to 'M' in the chrom column.")
 
-    def check_chrom_set(self, input_df):
-        # chrom_cols = sorted(input_df.select('chrom').distinct().collect()[1])
-        chrom_cols = sorted([int(x) for x in input_df['chrom'].unique()])
-        self.assertItemsEqual(self.somatic_chr, sorted(chrom_cols),
-                              "Not all somatic chromosomes are loaded {}.".format(chrom_cols))
+    def alt_check(self):
+        result = self.sqlContext.sql("""
+        SELECT alt
+        FROM spark_df
+        WHERE regexp_extract( alt, '([^ACGTN.])', 0 ) IS NULL
+        """)
+        self.assertTrue(result.count() == 0, "The ref field contains values other than A,T,G,C or N.")
 
-    def check_chrom_count(self, input_df):
-        for name, group in input_df['chrom'].groupby(input_df['chrom']):
-            self.assertGreater(group.count(), 999, "Chrom {} only has {} rows.".format(name, group.count()))
+    def test_alleles(self):
+        result = self.sqlContext.sql("""
+        SELECT chrom, pos, ref, alt, subject_id, sample_id
+        FROM spark_df
+        WHERE length(ref) = length(alt)
+        AND alt <> '.'
+        GROUP BY chrom, pos, ref, alt, subject_id, sample_id
+        HAVING COUNT(*) > 2
+        """)
+        self.assertTrue(result.count() == 0, "The alt field contains values other than A,T,G,C or N.")
 
-    def check_chrom_prefix(self, input_df):
-        chroms = input_df['chrom'].unique()
-        chrom_string = ''.join(str(e) for e in chroms)
-        self.assertNotIn(chrom_string, 'chr')
+    def test_ref(self):
+        result = self.sqlContext.sql("""
+            SELECT chrom, pos, ref
+            FROM spark_df
+            WHERE length(ref) = length(alt)
+            AND ref <> '.'
+            GROUP BY chrom, pos, ref
+            HAVING COUNT(*) > 2
+            """)
+        self.assertTrue(result.count() == 0, "Multiple values for the reference field were found at: {}".format(result))
 
-    def check_chrom_mt(self, input_df):
-        chroms = input_df['chrom'].unique()
-        chrom_string = ''.join(str(e) for e in chroms)
-        self.assertNotIn(chrom_string, 'mt')
-
-    def ref_check(self, input_df):
-        ref_col = input_df['ref'].unique()
-        ref_set = set(''.join(sorted(ref_col)))
-        result = [s for s in ref_set if s.strip('ATGCN')]
-        assert len(result) == 0, "The following values were found in the ref field: {}".format(result)
-
-    def test_alleles(self, input_df):
-        no_indels = input_df[input_df['alt'].str.len() == input_df['ref'].str.len()]
-        pos_group = no_indels.groupby(['subject_id', 'chrom', 'pos', 'ref'])
-        problems = pos_group.filter(lambda x: len(x) > 2)
-        assert len(problems) == 0, "Found more than two alleles for the following subject/position: \n {}".format(problems)
-
-    def test_ref(self, input_df):
-        ref_set = input_df[['chrom', 'pos', 'ref']][(input_df['ref'] != '.') and (input_df[input_df['alt'].str.len() == input_df['ref'].str.len()])]
-        ref_group = ref_set.groupby(['chrom', 'pos'])
-        for name, group in ref_group:
-            assert len(set(group['ref'])) < 2, "Different reference alleles were found at {}".format(name)
-
-    def get_ref_snp(self, input_df):
-        random_snp = input_df[['chrom', 'pos', 'ref', 'alt']][input_df['ref'] != '.'].sample(1)
-        return random_snp
-
-    def query_snp(self, input_snp):
-        exclude = ['ins', 'del']
-        query_string = "chr{}:{}".format(input_snp.iloc[0]['chrom'], input_snp.iloc[0]['pos'])
-        test_url = 'http://myvariant.info/v1/query?q={}'.format(query_string)
-        response = urllib.urlopen(test_url)
-        data = json.loads(response.read())
-        ref_var = []
-        while len(ref_var) == 0:
-            for x in range(0,4):
-                try:
-                     ref_hit = data['hits'][0]['_id'] if len(data['hits'][0]['_id']) > 0 else 'null'
-                     if any(x in exclude for x in ref_hit):
-                         pass
-                except:
-                     time.sleep(10)
-            else:
-                ref_var.append(ref_hit)
-                ref_out = ''.join(ref_var)
-                return ref_out
-
-    def check_one_based(self, input_df):
-        test_snp = self.get_ref_snp(input_df)
-        ref_snp = self.query_snp(test_snp)
-        file_snp =  "chr{}:g.{}{}>{}".format(test_snp.iloc[0]['chrom'], test_snp.iloc[0]['pos'],  test_snp.iloc[0]['ref'],  test_snp.iloc[0]['alt'])
-        self.assertEqual(ref_snp, file_snp, "File variant {} does not match reference {}".format(file_snp, ref_snp))
+    # def get_ref_snp(self, input_df):
+    #     random_snp = input_df[['chrom', 'pos', 'ref', 'alt']][input_df['ref'] != '.'].sample(1)
+    #     return random_snp
+    #
+    # def query_snp(self, input_snp):
+    #     exclude = ['ins', 'del']
+    #     query_string = "chr{}:{}".format(input_snp.iloc[0]['chrom'], input_snp.iloc[0]['pos'])
+    #     test_url = 'http://myvariant.info/v1/query?q={}'.format(query_string)
+    #     response = urllib.urlopen(test_url)
+    #     data = json.loads(response.read())
+    #     ref_var = []
+    #     while len(ref_var) == 0:
+    #         for x in range(0,4):
+    #             try:
+    #                  ref_hit = data['hits'][0]['_id'] if len(data['hits'][0]['_id']) > 0 else 'null'
+    #                  if any(x in exclude for x in ref_hit):
+    #                      pass
+    #             except:
+    #                  time.sleep(10)
+    #         else:
+    #             ref_var.append(ref_hit)
+    #             ref_out = ''.join(ref_var)
+    #             return ref_out
+    #
+    # def check_one_based(self, input_df):
+    #     test_snp = self.get_ref_snp(input_df)
+    #     ref_snp = self.query_snp(test_snp)
+    #     file_snp =  "chr{}:g.{}{}>{}".format(test_snp.iloc[0]['chrom'], test_snp.iloc[0]['pos'],  test_snp.iloc[0]['ref'],  test_snp.iloc[0]['alt'])
+    #     self.assertEqual(ref_snp, file_snp, "File variant {} does not match reference {}".format(file_snp, ref_snp))
 
     def tear_down(self):
         # close connection
@@ -141,37 +177,37 @@ class TestETL(unittest.TestCase):
         pd_df = self.sparkDf_to_pandasDf(file_df)
 
         # check chrom, pos, sample_id column vals not empty
-        self.check_chrom_empty(file_df)
-        self.check_ref_empty(file_df)
-        self.check_subject_empty(file_df)
-        self.check_allele_empty(file_df)
-        self.check_sample_empty(file_df)
+        self.check_empty("chrom")
+        self.check_empty("ref")
+        self.check_empty("subject_id")
+        # self.check_empty("allele")
+        self.check_empty("sample_id")
 
         # check that all somatic chroms loaded
-        self.check_chrom_set(pd_df)
+        self.check_chrom_set()
 
         # check that each chrom contains at least 1000 rows
-        self.check_chrom_count(pd_df)
+        self.check_chrom_count()
 
         # check that 'chr' prefix has been removed from chromosome
-        self.check_chrom_prefix(pd_df)
+        self.check_chrom_prefix()
 
-        # ref cols contains expected values A T G C N and are uppercase ###
-        self.ref_check(pd_df)
+        # check that mitochondrial chromosome has been renamed from MT to M
+        self.check_chrom_mt()
 
-        # check for no more than two alleles per person at chrom, pos, ref
-        self.test_alleles(pd_df)
+        # alt cols contains expected values A T G C N only
+        self.alt_check()
 
-        # # ref allele should be the same at each position
-        self.test_ref(pd_df)
+        # # check for no more than two alleles per person at chrom, pos, ref
+        self.test_alleles()
+        #
+        # # # ref allele should be the same at each position
+        self.test_ref()
 
         # verify 1-based coords
         # self.check_one_based(pd_df)
 
         self.tear_down()
-
-
-
 
 
 ###############
@@ -180,9 +216,8 @@ if __name__ == '__main__':
     # instantiate class with current wd, hdfs dir, spark evn, spark job name, num cores
     spark = TestETL(os.getcwd(), '/user/selasady/', "local", "etl_test", 10)
 
-
     # specify input file to process
-    tsv_infile = '/user/selasady/test_set.txt'
+    tsv_infile = '/user/selasady/test_set.txt.bz2'
 
     # run tests
     spark.run_tests(tsv_infile)
