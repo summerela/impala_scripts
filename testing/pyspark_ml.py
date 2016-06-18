4bddd2b3c00181111735bd736a58249bc66bdda7
summerela <summerrae78@gmail.com>
2016-06-17 17:49:24 -0700

    added ml functions, transform sql query rdd for scaling

--- testing/pyspark_ml.py
+++ testing/pyspark_ml.py
@@ -2,18 +2,30 @@
 
 import os
 from pyspark import SparkContext, SparkConf, SQLContext
-from pyspark.mllib.tree import RandomForest, RandomForestModel
 from pyspark.mllib.linalg import Vectors
 from pyspark.mllib.feature import StandardScaler
 from pyspark.mllib.util import MLUtils
-from pyspark.sql.types import *
+from pyspark.mllib.classification import SVMWithSGD
+from pyspark.mllib.regression import LabeledPoint, LinearRegressionWithSGD
+from pyspark.mllib.evaluation import RegressionMetrics
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+from pyspark.mllib.classification import NaiveBayes
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import col
 
-class pyspark_ml():
 
-    def __init__(self, local_dir='./', hdfs_dir='/titan/ITMI1/workspaces/users/',
-                 master='local', appname='spark_app', spark_mem=2):
+
+
+class pyspark_classify():
+
+    def __init__(self, in_file, num_iterations=100, local_dir=os.getcwd(), hdfs_dir='/vlad/',
+                 master='local', appname='spark_ml', spark_mem=4):
+        self.in_file = in_file
         self.local_dir = local_dir
         self.hdfs_dir = hdfs_dir
+        # number of times to iterate through training algorithm
+        self.num_iterations = num_iterations
+        # spark connection string options
         self.master = master
         self.appname = appname
         self.spark_mem = int(spark_mem)
@@ -22,74 +34,103 @@ class pyspark_ml():
                      .setAppName(self.appname)
                      .set("spark.executor.memory", self.spark_mem))
         self.sc = SparkContext(conf=self.conf)
-        self.somatic_chr = sorted(map(int, range(1, 23)))
+        # create sql context submodule for extra features
         self.sqlContext = SQLContext(self.sc)
+        # read in parquet format data from hdfs
+        self.data = self.sqlContext.read.parquet("{}".format(in_file))
+        self.label = self.data.map(lambda x: x.label)
+        self.features = self.data.map(lambda x: x[['qual', 'filter', 'rsid']])
+        # create a set of labeled points to pass to svm model
+        # self.labeled_points = self.data.map(lambda row: LabeledPoint(row[0], row[1:]))
 
-    def tsv_to_libsvm(self, in_file):
-        df = self.sqlContext.read.load(in_file,
-                                  format='com.databricks.spark.csv',
-                                  header='true',
-                                  inferSchema='true',
-                                  delimiter='\t' )
-        print df.take(10)
+    def scale_split(self):
+        # create scaler model to transform data to mean of 0 and scale data by stdev
+        scaler = StandardScaler(withMean=True, withStd=True).fit(self.features)
+        # apply scaling to data features
+        scalerModel = scaler.fit(self.data)
+        scaledData = scalerModel.transform(self.data)
+        # create training and test set
+        trainingData, testData = scaledData.randomSplit([0.7, 0.3])
+        # cach the training data for speed
+        trainingData.cache()
+        return trainingData, testData
 
-    def scale_features(self, in_file):
-        data = MLUtils.loadLibSVMFile(self.sc, in_file)
-        label = data.map(lambda x: x.label)
-        features = data.map(lambda x: x.features)
+    def getMSE(self, testingData, model):
+        # get mean squared error for each model
+        valuesAndPreds = testingData.map(lambda p: (p.label, model.predict(p.features[0])))
+        MSE = valuesAndPreds.map(lambda (v, p): (v - p) ** 2).reduce(lambda x, y: x + y) / valuesAndPreds.count()
+        return MSE
 
-        scaler1 = StandardScaler().fit(features)
-        scaler2 = StandardScaler(withMean=True, withStd=True).fit(features)
+    def run_svm(self, training_data):
+        # Build the model
+        svm_model = SVMWithSGD.train(training_data, iterations=int(self.num_iterations))
+        print("Mean Squared Error = " + str(self.getMSE(svm_model)))
+        return svm_model
 
-        # data1 will be unit variance.
-        data_var = label.zip(scaler1.transform(features))
+    def run_linear_regression(self, training_data):
+        linear_regression_model = LinearRegressionWithSGD.train(training_data, iterations=self.num_iterations, step=0.1)
+        return linear_regression_model
 
-        # Without converting the features into dense vectors, transformation with zero mean will raise
-        # exception on sparse vector.
-        # data2 will be unit variance and zero mean.
-        data_mean = label.zip(scaler2.transform(features.map(lambda x: Vectors.dense(x.toArray()))))
+    def run_logistic_regression(self, training_data):
+        logistic_regression_model = LogisticRegressionWithLBFGS.train(training_data, iterations=self.num_iterations)
+        return logistic_regression_model
 
-        return data_var, data_mean
-
-
-
+    def run_naive_bayes(self, training_data):
+        bayes_model = NaiveBayes.train(training_data, lambda_=1.0)
+        return bayes_model
 
 
-    def random_forest(self, in_file):
-        data = MLUtils.loadLibSVMFile(self.sc, in_file)
-        # Split the data into training and test sets (30% held out for testing)
-        (trainingData, testData) = data.randomSplit([0.7, 0.3])
-        # Train a RandomForest model.
-        #  Empty categoricalFeaturesInfo indicates all features are continuous.
-        model = RandomForest.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
-                                             numTrees=3, featureSubsetStrategy="auto",
-                                             impurity='gini', maxDepth=4, maxBins=32)
 
-        # Evaluate model on test instances and compute test error
-        predictions = model.predict(testData.map(lambda x: x.features))
-        labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
-        testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
-        print('Test Error = ' + str(testErr))
-        print('Learned classification forest model:')
-        print(model.toDebugString())
+
+
 
 
 ##########################
 if __name__ == '__main__':
 
-    # instantiate class with current wd, hdfs dir, spark evn, spark job name, num cores
-    spark = pyspark_ml(os.getcwd(), '/user/selasady/', "local", "etl_test", 10)
-
-    # specify name of input file on hdfs
-    tsv_infile = 'sample_libsvm_data.txt'
-
-    # parse vcf
-    spark.tsv_to_libsvm(tsv_infile)
-
-    data_var, data_mean = spark.scale_features(tsv_infile)
+    # instantiate class with input data file, num of iterations, current wd, hdfs dir, spark evn, spark job name, num cores
+    spark = pyspark_classify('hdfs://nameservice1/vlad/wgs_ilmn.db/svm_parquet', 100, os.getcwd(), '/vlad/', "local", "etl_test", 10)
 
 
-    print data_var.take(10)
+    # parsedData = spark.parse_data()
+    #
+    # print parsedData.take(2)
 
-    # run random forest
-    # spark.random_forest(tsv_infile)
+
+
+
+
+
+    # train, test = spark.scale_split()
+    #
+    # spark.run_svm(train)
+
+
+
+    # def tsv_to_libsvm(self, in_file):
+    #     df = self.sqlContext.read.load(in_file,
+    #                               format='com.databricks.spark.csv',
+    #                               header='false',
+    #                               inferSchema='true',
+    #                               comment = '#',
+    #                               delimiter='\t' )
+    #     return df
+
+    # from pyspark.mllib.tree import RandomForest, RandomForestModel
+    # def random_forest(self, in_file):
+    #     data = MLUtils.loadLibSVMFile(self.sc, in_file)
+    #     # Split the data into training and test sets (30% held out for testing)
+    #     (trainingData, testData) = data.randomSplit([0.7, 0.3])
+    #     # Train a RandomForest model.
+    #     #  Empty categoricalFeaturesInfo indicates all features are continuous.
+    #     model = RandomForest.trainClassifier(trainingData, numClasses=2, categoricalFeaturesInfo={},
+    #                                          numTrees=3, featureSubsetStrategy="auto",
+    #                                          impurity='gini', maxDepth=4, maxBins=32)
+    #
+    #     # Evaluate model on test instances and compute test error
+    #     predictions = model.predict(testData.map(lambda x: x.features))
+    #     labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+    #     testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
+    #     print('Test Error = ' + str(testErr))
+    #     print('Learned classification forest model:')
+    #     print(model.toDebugString())
