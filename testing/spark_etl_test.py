b2a7f733f622ee72cb5fbb9930fa7a59473773f4
summerela <summerrae78@gmail.com>
2016-07-05 18:55:54 -0700

    updating etl testing for backwards compatiblity with itmi's servers

--- testing/spark_etl_test.py
+++ testing/spark_etl_test.py
@@ -2,22 +2,25 @@
 
 '''
 Assumptions:
-Input file is in impala table at least the following columns:
+Python 2.6 or above
+Spark 1.3.0 or above
+
+Input:
+Impala table in parquet format
+Table contains at least these columns:
 - chrom
 - pos
 - ref
 - alt
+
+If the vcf="yes" option is chosen, table must also contain these columns:
 - subject_id
 - sample_id
 
 To Run:
 
-- Edit main routine
-
-Type the following from command line
-
-pyspark spark_etl_test.py  2>/dev/null
-
+- Edit main routine to instantiate class with correct table/options
+- Type the following from command line: pyspark spark_etl_test.py  2>/dev/null
 
 '''
 
@@ -33,7 +36,9 @@ from pyspark import SparkContext, SparkConf, SQLContext
 
 class TestETL(unittest.TestCase):
 
-    def __init__(self, in_table, is_vcf='no', platform='illumina', local_dir='./', hdfs_dir='/vlad/wgs_ilmn.db', appname='spark_app'):
+    def __init__(self, server_prefix='hdfs://nameservice1/vlad/', in_table = 'wgs_ilmn.db/vcf_variant', is_vcf='no', platform='illumina',
+    local_dir='./', hdfs_dir='/vlad/', appname='etl_testing'):
+        self.server_prefix = server_prefix
         self.in_table = in_table
         self.is_vcf = is_vcf
         self.platform = platform
@@ -43,32 +48,33 @@ class TestETL(unittest.TestCase):
         self.conf = (SparkConf()
                .setAppName(self.appname))
         self.sc = SparkContext(conf=self.conf)
-        self.somatic_chr = sorted(map(str, range(1,23) + ["M", "X", "Y"]))
+        self.chroms = sorted(map(str, range(1,23) + ["M", "X", "Y"]))
         self.sqlContext = SQLContext(self.sc)
         # encode any parquet binary formatting as strings
         self.sqlContext.sql("SET spark.sql.parquet.binaryAsString=true")
-        # # cache table metadata for query speed
+        # cache table metadata for query speed
         self.sqlContext.sql("SET spark.sql.parquet.cacheMetadata=true")
-        self.data = self.sqlContext.read.parquet("{}".format(self.in_table))
+        self.read_table = "%(server)s%(table)s" % {"server": self.server_prefix, "table": self.in_table}
+        # self.data = self.sqlContext.parquetFile(self.read_table)
+        self.data = self.sqlContext.read.parquet(self.read_table)
         # register parquet file as temp table to query
         self.parquet_tbl = self.data.registerTempTable("parquet_tbl")
 
 
     def check_empty(self, col_name):
-        # check for null values
-        result = self.sqlContext.sql("SELECT * FROM parquet_tbl WHERE {} IS NULL".format(col_name)).count()
-        self.assertTrue(result == 0, "Null ?values found in {} column.".format(col_name))
+        # Check that important columns do not contain null values
+        result = self.sqlContext.sql("SELECT * FROM parquet_tbl WHERE '%(table)s' IS NULL" % {'table' : col_name}).count()
+        self.assertTrue(result == 0, "Null values found in '%(column)s' column." % {'column' : col_name})
 
     def check_chrom_set(self):
-        # check that all chroms are loaded
+        # check that all chromosomes were loaded
         result = self.sqlContext.sql("SELECT distinct chrom FROM parquet_tbl")
         uploaded_chroms = sorted(map(str, result.rdd.map(lambda r: r.chrom).collect()))
-        self.assertListEqual(self.somatic_chr, uploaded_chroms,
-                             "The following chromosomes are not loaded: \n {}".format(
-                                 set(self.somatic_chr).difference(uploaded_chroms)))
+        self.assertEqual(self.chroms, uploaded_chroms,
+                             "Not all chromosomes were uploaded.")
 
     def check_chrom_count(self):
-        # check that data was uploaded for each chrom
+        # check that sufficient data was uploaded for each chrom
         result = self.sqlContext.sql("SELECT chrom, COUNT(*) FROM parquet_tbl GROUP BY chrom HAVING COUNT(*) < 10000")
         self.assertTrue(result.count() == 0, "Not all chromosomes contain at least 10000 rows.")
 
@@ -96,28 +102,29 @@ class TestETL(unittest.TestCase):
         self.assertTrue(result.count() == 0, "More than two alleles found per locus.")
 
     def subjects_loaded(self):
+        # Check that all subject id's were loaded by comparing table with metadata from redshift
         if str.lower(self.platform) == 'cgi':
-            vcf_file = 'hdfs://nameservice1/vlad/wgs_cg.db/vcf_file_test/'
-            file_tbl = self.sqlContext.read.parquet("{}".format(vcf_file))
+            vcf_file = '%(server_prefix)s/wgs_cg.db/vcf_file/' % {'server_prefix': self.server_prefix}
+            file_tbl = self.sqlContext.parquetFile("%(table)s" % {'table': vcf_file})
             # register parquet file as temp table to query
             vcf_file_tbl = file_tbl.registerTempTable("vcf_file_tbl")
-            result = self.sqlContext.sql("WITH a AS (SELECT DISTINCT subject_id FROM parquet_tbl) \
-                                         SELECT a.*, b.subject_id \
-                                         FROM a \
-                                         FULL OUTER JOIN vcf_file_tbl b \
+            subjects = self.sqlContext.sql("SELECT DISTINCT subject_id FROM parquet_tbl").collect()
+            result = self.sqlContext.sql("SELECT a.*, b.subject_id \
+                                         FROM subjects a \
+                                         FULL OUTER JOIN vcf_file_table b \
                                          ON a.subject_id = b.subject_id \
                                          WHERE (a.subject_id IS NULL or b.subject_id IS NULL)")
             self.assertTrue(result.count() == 0,
                             "All subject id's were not found in both tables.")
 
         elif str.lower(self.platform) == 'illumina':
-            vcf_file = 'hdfs://nameservice1/vlad/wgs_ilmn.db/vcf_file_test/'
-            file_tbl = self.sqlContext.read.parquet("{}".format(vcf_file))
+            vcf_file = '%(server_prefix)s/wgs_ilmn.db/vcf_file/' % {'server_prefix': self.server_prefix}
+            file_tbl = self.sqlContext.parquetFile("%(table)s" % {'table': vcf_file})
             # register parquet file as temp table to query
             vcf_file_tbl = file_tbl.registerTempTable("vcf_file_tbl")
-            result = self.sqlContext.sql("WITH a AS (SELECT DISTINCT subject_id FROM parquet_tbl) \
-                                         SELECT a.*, b.subject_id \
-                                         FROM a \
+            subjects = self.sqlContext.sql("SELECT DISTINCT subject_id FROM parquet_tbl").collect()
+            result = self.sqlContext.sql("SELECT a.*, b.subject_id \
+                                         FROM subjets a \
                                          FULL OUTER JOIN vcf_file_tbl b \
                                          ON a.subject_id = b.subject_id \
                                          WHERE (a.subject_id IS NULL or b.subject_id IS NULL)")
@@ -128,12 +135,17 @@ class TestETL(unittest.TestCase):
             print("Please enter either platform='illumina' or platform='cgi' in your class argument.")
 
     def chrom_per_subject(self):
-        result = self.sqlContext.sql("with t as (select distinct subject_id, chrom \
-                                     from vcf_variant) \
-                                     select subject_id, count(*) as count from t \
+        # make sure that all chromosomes were uploaded for each subject id
+        subset_tbl = self.sqlContext.sql("SELECT DISTINCT subject_id, chrom from parquet_tbl").collect()
+        result = self.sqlContext.sql("select subject_id, count(*) as count from subset_tbl \
                                      group by subject_id having count < 25")
         self.assertTrue(result.count() == 0,
-                        "Not all chromosomes were loaded for the following subject_id's: \n {}".format(result))
+                        "Not all chromosomes were loaded for the following subject_id's: \n '%(results)s" % {'results': result})
+
+    def bracket_check(self):
+        # check that structural variants and no calls were not loaded by searching for bracket notation
+        result = self.sqlContext.sql("select * from parquet_tbl where instr(allele, '[') != 0 or instr(allele, ']') != 0")
+        self.assertTrue(result.count() == 0, "Brackets were found in the allele field.")
 
     def tear_down(self):
         # clear memory cache
@@ -148,62 +160,43 @@ class TestETL(unittest.TestCase):
         except Exception as e:
             print e
 
-        # listing each test instead of using run_main
+        # list each test instead of using run_main
         # for ease of customization
-
-        # check chrom, pos, sample_id column vals not empty
         self.check_empty("chrom")
         self.check_empty("ref")
-        self.check_empty("subject_id")
-        self.check_empty("sample_id")
-
-        # check that all chroms loaded
         self.check_chrom_set()
-
-        # check that each chrom contains at least 10000 rows
         self.check_chrom_count()
-
-        # check that 'chr' prefix has been removed from chromosome
         self.check_chrom_prefix()
-
-        # check that mitochondria has been renamed from MT to M
         self.check_chrom_mt()
-
-         # check for no more than two alleles per person at each locus
-        self.test_alleles()
-
 
 ###############
 if __name__ == '__main__':
 
     # instantiate class
-<<<<<<< HEAD
-    spark = TestETL(in_table=, is_vcf='yes', 'hdfs://ip-10-0-0-118.ec2.internal:8020/itmi/wgs_cg.db/vcf_variant/'
-=======
-    spark = TestETL(in_table='hdfs://nameservice1/vlad/wgs_ilmn.db/vcf_variant/', is_vcf='yes',
->>>>>>> 97b9e513c07b98b63ca24ec1fa41d26cbc3dd4ae
-                    platform='illumina', local_dir=os.getcwd(), hdfs_dir='/user/selasady/', appname='vcf_testing')
+    spark = TestETL(server_prefix = 'hdfs://nameservice1/vlad/', in_table='wgs_ilmn.db/vcf_variant', is_vcf='yes',  \
+    platform='illumina', local_dir=os.getcwd(), hdfs_dir='/user/selasady/', appname='etl_testing')
 
     if spark.is_vcf == 'yes':
         # run tests
-        spark.run_tests()
+        # spark.run_tests()
         # check that all subjects listed in the vcf_file metadata were loaded
         spark.subjects_loaded()
+        spark.check_empty("subject_id")
+        spark.check_empty("sample_id")
         spark.chrom_per_subject()
+        spark.test_alleles()
+        print ("Unit tests passed.")
         spark.tear_down()
     else:
         # run tests
         spark.run_tests()
-        print ("Unit tests complete.")
+        print ("Unit tests passed.")
         spark.tear_down()
 
 
 
-# /itmi/wgs_cg.db/vcf_nocall
-<<<<<<< HEAD
-# /itmi/wgs_cg.db/vcf_variant
-# 'hdfs://ip-10-0-0-118.ec2.internal:8020/itmi/wgs_cg.db/vcf_variant/'
+# 'hdfs://ip-10-0-0-118.ec2.internal:8020/itmi/wgs_ilmn.db/vcf_variant/'
 # 'hdfs://nameservice1/vlad/wgs_ilmn.db/vcf_variant/'
-=======
-# /itmi/wgs_cg.db/vcf_variant
->>>>>>> 97b9e513c07b98b63ca24ec1fa41d26cbc3dd4ae
+# 'hdfs://ip-10-0-0-118.ec2.internal:8020/itmi/wgs_cg.db/vcf_variant'
+
+
