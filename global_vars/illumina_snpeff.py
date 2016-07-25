b9042e5cd7ab65eb82b69b2082ca1807ab095d73
summerela <summerrae78@gmail.com>
2016-07-25 16:26:42 -0700

    testing

--- global_vars/illumina_snpeff.py
+++ global_vars/illumina_snpeff.py
@@ -224,14 +224,14 @@ class snpeff(object):
     ##################
 
     def run_snpeff_pipeline(self):
-        # self.make_hdfs_dir()
+        self.make_hdfs_dir()
         for chrom in self.chroms:
             print ("Running snpeff on chromosome {} \n".format(chrom))
-            # self.run_snpeff(chrom)
-            # self.parse_snpeff(chrom)
-            # self.parse_tsv(chrom)
-            # self.upload_hdfs(chrom)
-            # self.remove_final(chrom)
+            self.run_snpeff(chrom)
+            self.parse_snpeff(chrom)
+            self.parse_tsv(chrom)
+            self.upload_hdfs(chrom)
+            self.remove_final(chrom)
         self.cur.close()
 
 
