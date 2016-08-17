cc5a9e9ef3669fb84f6633f95e390e16e4176c67
summerela <summerrae78@gmail.com>
2016-08-17 14:01:49 -0700

    adding pyspark sneff pipeline

--- global_vars/gv.py
+++ global_vars/gv.py
@@ -2,6 +2,7 @@
 
 from pyspark import SparkContext, SparkConf, SQLContext
 import subprocess as sp
+import os
 
 class gv(object):
 
@@ -25,6 +26,8 @@ class gv(object):
         self.sqlC = SQLContext(self.sc)
         self.sqlC.sql("SET spark.sql.parquet.binaryAsString=true")
         self.sqlC.sql("SET spark.sql.parquet.cacheMetadata=true")
+        self.var_df = self.sqlC.parquetFile("{}{}{}".format(self.spark_host_prefix, self.ilmn_db, 'vcf_distinct'))
+        self.var_tbl = self.var_df.registerTempTable("var_tbl")
 
     def register_table(self, prefix, in_table):
         print("Registering spark temp table {}...".format(in_table))
@@ -46,6 +49,11 @@ class gv(object):
         except sp.CalledProcessError as e:
             print e
 
+    @staticmethod
+    def check_outdir(output_dir):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
     def run_query(self, input_query):
         self.sqlC.sql(input_query)
 
@@ -53,11 +61,38 @@ class gv(object):
         self.sqlC.clearCache()
         self.sc.stop()
 
+    def run_snpeff(self, input_chrom):
+        '''
+        Run snpeff by chromosome on ilmn_vars table
+        :return: vcf files of annoated variants for each chrom
+        '''
+        # select variants by chromosome
+        get_vars_query = "SELECT chrom as '#chrom', pos, var_id as id, ref, allele as alt, 100 as qual, \
+                         'PASS' as filter, 'GT' as 'format', '.' as INFO from wgs_ilmn.ilmn_vars \
+                         where chrom = '{}'".format(input_chrom)
+        var_df = self.pandas_query(get_vars_query)
+        # run snpeff on query results
+        if not var_df.empty:
+            snp_out = "{}/chr{}_snpeff.vcf".format(self.out_dir, input_chrom)
+            snpeff_cmd = r'''java -d64 -Xmx4g -jar {snpeff} -t -v GRCh37.75 > {vcf_out}'''.format(snpeff=self.snpeff_jar,
+                                                                                              vcf_out=snp_out)
+            # run the subprocess command
+            ps = sp.Popen(snpeff_cmd, shell=True, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, cwd=os.getcwd())
+            stdout, stderr = ps.communicate(var_df.to_csv(sep='\t', header=True, index=False))
+            if stdout:
+                logger.info(stdout)
+            if stderr:
+                logger.error(stderr)
+                print("Error encountered on chrom {}, check snpeff.log".format(input_chrom))
+
+        else:
+            print("No variants found for chromosome {}".format(input_chrom))
 
 ############################################
 if __name__ == 'main':
 
     gv = gv()
 
+    gv.var_df.show()
 
     gv.shut_down()
