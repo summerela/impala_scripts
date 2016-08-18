5cfb6ffd796b3d345a183088614cf1a33e30011f
summerela <summerrae78@gmail.com>
2016-08-18 16:15:45 -0700

    re-writing run_snpeff() to use RDD pipe to subprocess

--- global_vars/gv.py
+++ global_vars/gv.py
@@ -88,7 +88,8 @@ class snpeff(object):
         snp_out = "{}/chr{}_snpeff.vcf".format(self.out_dir, input_chrom)
         snpeff_cmd = r'''java -d64 -Xmx32g -jar {snpeff} -t -v GRCh37.75 > {vcf_out}'''.format(snpeff=self.snpeff_jar,
                                                                                           vcf_out=snp_out)
-        print var_df.take(10)
+        test = var_df.map(lambda line: line.split('\t'))
+        print test.take(5)
 
         #stuff
         # run the subprocess command
